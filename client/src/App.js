a39b41d6a82cbf44cebd6aeaf07a395f6ad92d10
brent <brent.lawson@insight.com>
2020-10-22 08:53:33 -0400

    private route

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,9 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
+
 
 import Login from './components/auth/Login';
 // Redux
@@ -33,6 +36,7 @@ const App = () => {
         <Switch>
           <Route exact path="/register" component={Register} />
           <Route exact path="/login" component={Login} />
+          <PrivateRoute exact path="/dashboard" component={Dashboard} />
         </Switch>
       </section>
     </Fragment>
