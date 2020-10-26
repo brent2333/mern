82d21d196c881ff3e5d91d28f41af2336883b0c4
brent <brent.lawson@insight.com>
2020-10-26 08:22:03 -0400

    more profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -3,17 +3,21 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
+import ProfileTop from './ProfileTop';
 import { getProfileById } from '../../actions/profile';
  
 const Profile = ({ getProfileById, profile: { profile, loading }, auth, match }) => {
     useEffect(() => {
         getProfileById(match.params.id)
-    },[getProfileById]);
+    },[getProfileById,match.params.id]);
     return (
             <Fragment>
                 {profile === null || loading ? <Spinner /> : <Fragment>
                     <Link to="/profiles" className="btn btn-light">Back to profiles</Link>
                     {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (<Link to='/edit-profile' className='btn btn-dark'>Edit Profile</Link>)}
+                    <div className="profile-grid my-1">
+                        <ProfileTop profile={profile}></ProfileTop>
+                    </div>
                     </Fragment>}
             </Fragment>  
     )
