72f3d4eafc5a97cf06e9b37265bf0b1fdf4b0afa
brent <brent.lawson@insight.com>
2020-10-27 13:58:46 -0400

    single post view

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST, ADD_POST } from './types';
+import { GET_POSTS, GET_POST, POST_ERROR, UPDATE_LIKES, DELETE_POST, ADD_POST } from './types';
 
 export const getPosts = () => async (dispatch) => {
   try {
@@ -82,6 +82,21 @@ export const deletePost = id => async dispatch => {
         payload: res.data
       });
       dispatch(setAlert('Post Created', 'success'));
+    } catch (err) {
+      dispatch({
+        type: POST_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+      });
+    }
+  };
+
+  export const getPost = id => async (dispatch) => {
+    try {
+      const res = await axios.get(`/api/posts/${id}`);
+      dispatch({
+        type: GET_POST,
+        payload: res.data
+      });
     } catch (err) {
       dispatch({
         type: POST_ERROR,
---
+++ client/src/components/post/Post.js
@@ -0,0 +1,24 @@
+import React, { Fragement, Fragment, useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux';
+import Spinner from '../../components/layout/Spinner';
+import { getPost } from  '../../actions/post';
+import PostItem from '../posts/PostItem';
+
+const Post = ({ getPost, post: { post, loading }, match }) => {
+    useEffect(() => {
+        getPost(match.params.id);
+    },[getPost])
+    return loading || post === null ? <Spinner /> : <Fragment>
+        <PostItem post={post} showActions={false} />
+    </Fragment>
+}
+
+Post.propTypes = {
+    getPost: PropTypes.func.isRequired,
+    post: PropTypes.object.isRequired,
+}
+const mapStateToProps = state => ({
+    post: state.post
+})
+export default connect(mapStateToProps, { getPost })(Post);
